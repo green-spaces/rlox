1a7983828b24be9da15e5d864b26dd2d73314c07
Matthew Taylor <mwtaylor902@gmail.com>
2023-07-22 13:48:17 -0400

    Added token types

--- src/token.rs
+++ src/token.rs
@@ -1,49 +1,75 @@
 #[derive(Debug)]
-pub struct Token {}
+pub struct Token {
+    t_type: TokenType,
+    lexeme: String,
+    literal: Literal,
+    line: u64,
+}
 
+impl Token {
+    fn new(t_type: TokenType, lexeme: String, literal: Literal, line: u64) -> Self {
+        Self {
+            t_type,
+            lexeme,
+            literal,
+            line,
+        }
+    }
+}
+
+impl std::fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("{:?} {} {:?}", self.t_type, self.lexeme, self.literal))
+    }
+}
+
+#[derive(Debug)]
+pub enum Literal {}
+
+#[derive(Debug)]
 pub enum TokenType {
     // Single-character tokens.
-    LEFT_PAREN,
-    RIGHT_PAREN,
-    LEFT_BRACE,
-    RIGHT_BRACE,
-    COMMA,
-    DOT,
-    MINUS,
-    PLUS,
-    SEMICOLON,
-    SLASH,
-    STAR,
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
     // One or two character tokens.
-    BANG,
-    BANG_EQUAL,
-    EQUAL,
-    EQUAL_EQUAL,
-    GREATER,
-    GREATER_EQUAL,
-    LESS,
-    LESS_EQUAL,
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
     // Literals.
-    IDENTIFIER,
-    STRING,
-    NUMBER,
+    Identifier,
+    String,
+    Number,
     // Keywords.
-    AND,
-    CLASS,
-    ELSE,
-    FALSE,
-    FUN,
-    FOR,
-    IF,
-    NIL,
-    OR,
-    PRINT,
-    RETURN,
-    SUPER,
-    THIS,
-    TRUE,
-    VAR,
-    WHILE,
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
 
-    EOF,
+    Eof,
 }
